036c959fab33ad8487947d68476116e25a812538
Oluwafemi <femikola23@gmail.com>
2021-12-01 14:14:31 +0400

    Update calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -14,35 +14,38 @@ const Calculator = () => {
   };
   return (
 
-    <div className="calculator disflex">
-      <div className="solution">
-        {total}
-        {operation}
-        {next}
-      </div>
-      <div className="buttons disflex">
-        <div className="grey-buttons">
-          <button type="button" onClick={() => onClickHandler('AC')}>AC</button>
-          <button type="button" onClick={() => onClickHandler('+/-')}>+/-</button>
-          <button type="button" onClick={() => onClickHandler('%')}>%</button>
-          <button type="button" onClick={() => onClickHandler('7')}>7</button>
-          <button type="button" onClick={() => onClickHandler('8')}>8</button>
-          <button type="button" onClick={() => onClickHandler('9')}>9</button>
-          <button type="button" onClick={() => onClickHandler('4')}>4</button>
-          <button type="button" onClick={() => onClickHandler('5')}>5</button>
-          <button type="button" onClick={() => onClickHandler('6')}>6</button>
-          <button type="button" onClick={() => onClickHandler('1')}>1</button>
-          <button type="button" onClick={() => onClickHandler('2')}>2</button>
-          <button type="button" onClick={() => onClickHandler('3')}>3</button>
-          <button type="button" onClick={() => onClickHandler('0')} className="double-btn">0</button>
-          <button type="button" onClick={() => onClickHandler('.')}>.</button>
+    <div className="main-calc disflex">
+      <p className="calc-text">Let&apos;s do some Math!</p>
+      <div className="calculator disflex">
+        <div className="solution">
+          {total}
+          {operation}
+          {next}
         </div>
-        <div className="orange-buttons disflex">
-          <button type="button" onClick={() => onClickHandler('÷')}>÷</button>
-          <button type="button" onClick={() => onClickHandler('x')}>x</button>
-          <button type="button" onClick={() => onClickHandler('-')}>-</button>
-          <button type="button" onClick={() => onClickHandler('+')}>+</button>
-          <button type="button" onClick={() => onClickHandler('=')}>=</button>
+        <div className="buttons disflex">
+          <div className="grey-buttons">
+            <button type="button" onClick={() => onClickHandler('AC')}>AC</button>
+            <button type="button" onClick={() => onClickHandler('+/-')}>+/-</button>
+            <button type="button" onClick={() => onClickHandler('%')}>%</button>
+            <button type="button" onClick={() => onClickHandler('7')}>7</button>
+            <button type="button" onClick={() => onClickHandler('8')}>8</button>
+            <button type="button" onClick={() => onClickHandler('9')}>9</button>
+            <button type="button" onClick={() => onClickHandler('4')}>4</button>
+            <button type="button" onClick={() => onClickHandler('5')}>5</button>
+            <button type="button" onClick={() => onClickHandler('6')}>6</button>
+            <button type="button" onClick={() => onClickHandler('1')}>1</button>
+            <button type="button" onClick={() => onClickHandler('2')}>2</button>
+            <button type="button" onClick={() => onClickHandler('3')}>3</button>
+            <button type="button" onClick={() => onClickHandler('0')} className="double-btn">0</button>
+            <button type="button" onClick={() => onClickHandler('.')}>.</button>
+          </div>
+          <div className="orange-buttons disflex">
+            <button type="button" onClick={() => onClickHandler('÷')}>÷</button>
+            <button type="button" onClick={() => onClickHandler('x')}>x</button>
+            <button type="button" onClick={() => onClickHandler('-')}>-</button>
+            <button type="button" onClick={() => onClickHandler('+')}>+</button>
+            <button type="button" onClick={() => onClickHandler('=')}>=</button>
+          </div>
         </div>
       </div>
     </div>
