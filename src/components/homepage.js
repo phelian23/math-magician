7b4044b4d8eec7829b959466195df4dcade26ecd
Oluwafemi <femikola23@gmail.com>
2021-12-01 14:14:51 +0400

    Add homepage

---
+++ src/components/homepage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './homepage.module.css';
+
+const Homepage = () => (
+  <header className={styles.header}>
+    <h3>Welcome to our page!</h3>
+    <p className={styles.text}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ullam modi
+      magni odit distinctio facilis tempora illum commodi
+      cum quasi ab ratione fuga inventore vero, consequuntur, consectetur eaque voluptatum vitae?
+      magni odit distinctio facilis tempora illum commodi
+      cum quasi ab ratione fuga inventore vero, consequuntur, consectetur eaque voluptatum vitae?
+      magni odit distinctio facilis tempora illum commodi
+      cum quasi ab ratione fuga inventore vero, consequuntur, consectetur eaque voluptatum vitae?
+    </p>
+    <p className={styles.text}>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium ullam modi
+      magni odit distinctio facilis tempora illum commodi cum quasi ab ratione fuga
+      inventore vero, consequuntur, consectetur eaque voluptatum vitae?
+      magni odit distinctio facilis tempora illum commodi
+      cum quasi ab ratione fuga inventore vero, consequuntur, consectetur eaque voluptatum vitae?
+      magni odit distinctio facilis tempora illum commodi
+      cum quasi ab ratione fuga inventore vero, consequuntur, consectetur eaque voluptatum vitae?
+    </p>
+  </header>
+);
+
+export default Homepage;
